546be5da08a4b3b81ddcd9b176df83cdfad55c43
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2025-01-17 22:01:54 +0000

    fix: update help messages for predefined times using localization

--- config.lua
+++ config.lua
@@ -22,10 +22,10 @@ Config.NextWeatherLogic = {
 }
 
 Config.PredefinedTimes = {
-    morning   = { hour = 9,  minute = 0, help = 'Set the time to 09:00' },
-    noon      = { hour = 12, minute = 0, help = 'Set the time to 12:00' },
-    evening   = { hour = 18, minute = 0, help = 'Set the time to 18:00' },
-    night     = { hour = 23, minute = 0, help = 'Set the time to 23:00' },
+    morning   = { hour = 9,  minute = 0, help = Lang:t('help.morningcommand') },
+    noon      = { hour = 12, minute = 0, help = Lang:t('help.nooncommand') },
+    evening   = { hour = 18, minute = 0, help = Lang:t('help.eveningcommand') },
+    night     = { hour = 23, minute = 0, help = Lang:t('help.nightcommand') },
 }
 
 Config.AvailableWeatherTypes = { -- DON'T TOUCH UNLESS YOU KNOW WHAT YOU ARE DOING
